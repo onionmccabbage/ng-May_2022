3e174a80762e6c635b4266ccca3db7642ba1b536
onionmccabbage@live.com <onionmccabbage@live.com>
2022-05-03 14:54:07 +0100

    day 1 review

--- src/app/review/review.component.ts
+++ src/app/review/review.component.ts
@@ -10,6 +10,15 @@ export class ReviewComponent implements OnInit {
   @Input() imgSize = 12
   @Input() init
 
+
+  // an array of image model data
+  pictures = [
+    {imgUrl:'https://placekitten.com/240/120', w:120, alt:'kitten', title:'hello kitty'},
+    {imgUrl:'https://placebear.com/240/120', w:120, alt:'bear', title:'lions and tigers and...'},
+    {imgUrl:'https://placekitten.com/120/120', w:120, alt:'other', title:'awwww'},
+    {imgUrl:'https://placekitten.com/320/120', w:120, alt:'more', title:'cute'}]
+
+
   ngOnInit() {
   }
   grow = ()=>{
@@ -23,5 +32,11 @@ export class ReviewComponent implements OnInit {
     if (Number(this.init.w) - 32 >= 32){
       this.init.w = Number(this.init.w) - 32
     }
+  }
+  removeImage = (whichImage)=>{ // receive an index number - which image was clicked?
+    // this.toDoList.indexOf(addedItem)
+    this.monitor=whichImage
+    let index = Number(whichImage)
+    this.pictures.splice(index,1)
   }
 }
